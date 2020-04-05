b489223bdc56f003983d1708a4aec93a242eb53d
HT <htibrewal7081@gmail.com>
2020-04-05 16:57:38 +0530

    first apk

--- lib/activities/newHome.dart
+++ lib/activities/newHome.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:shadow/shadow.dart';
-
 import '../services/routers.dart';
 
 class NewHome extends StatelessWidget {
@@ -23,7 +21,7 @@ class NewHome extends StatelessWidget {
             children: <Widget>[
               InkWell(
                 onTap: () {
-                  Navigator.pushNamed(context, '/saleshome');
+                  Navigator.pushNamed(context, '/salesHome');
                 },
                 child: Container(
                   margin: EdgeInsets.only(left: 10.0, right: 10.0),
@@ -74,8 +72,9 @@ class NewHome extends StatelessWidget {
                       Container(
                         width: 200,
                         height: 200.0,
+                        child: Image.network(
+                            'https://cdn4.iconfinder.com/data/icons/flat-business-icon-set/450/bar_chart-512.png'),
                         decoration: BoxDecoration(
-                            color: Colors.grey,
                             borderRadius: BorderRadius.only(
                                 bottomRight: Radius.circular(20.0),
                                 topRight: Radius.circular(20.0))),
@@ -85,21 +84,72 @@ class NewHome extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 50.0,
+                height: 30.0,
               ),
-              Container(
-                margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                height: 200.0,
-                decoration: BoxDecoration(
-                    color: Color(0xff141d26),
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey,
-                          spreadRadius: 1.0,
-                          blurRadius: 1.0)
-                    ]),
-              )
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/transit');
+                },
+                child: Container(
+                  margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      color: Color(0xff141d26),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            spreadRadius: 1.0,
+                            blurRadius: 1.0)
+                      ]),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Pie Chart',
+                            style: TextStyle(
+                                color: Colors.orange,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '\tManHours',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20.0),
+                          ),
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          Center(
+                              child: Text(
+                            'Tap to see Chart',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                          SizedBox(
+                            height: 20.0,
+                          )
+                        ],
+                      ),
+                      Container(
+                        width: 200,
+                        height: 200.0,
+                        child: Image.network(
+                            'https://cdn1.iconfinder.com/data/icons/charts-and-diagrams-1-1/512/piechart-512.png'),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(20.0),
+                                topRight: Radius.circular(20.0))),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
